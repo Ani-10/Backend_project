ba61095c538a515f0099fbb8894ad78f91443647
Ani-10 <sundar1anindya2@gmail.com>
2022-11-14 22:25:54 +0530

    changing the port no for weather app

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -41,6 +41,6 @@ app.post("/", function(req,res){
 
 
 
-app.listen(3000, function(){
-  console.log("Server is running on port 3000.");
+app.listen(4500, function(){
+  console.log("Server is running on port 4500.");
 })
